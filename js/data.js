12e9dbffcb833df06b2477da43d4ef6af42e6bb0
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-17 22:18:07 +0000

    finished typescript part of issue 1

--- js/data.js
+++ js/data.js
@@ -1,31 +1,15 @@
-interface Entry {
-  entryID: number;
-  imageUrl: string;
-  title: string;
-  notes: string;
-}
-
-interface Data {
-  view: 'entries' | 'entry-form';
-  entries: Entry[];
-  editing: null | Entry;
-  nextEntryId: number;
-}
-
-const data: Data = readDataModel();
-
+'use strict';
+const data = readDataModel();
 // In data.ts, add functions that serialize the data model
 // as JSON and stores the JSON in localStorage, and that reads
 // the JSON from localStorage returned the parsed value
 // (or a default data model if it does not exist).
-
-function serializeDataModel(): void {
+function serializeDataModel() {
   const serializeData = JSON.stringify(data);
   localStorage.setItem('data-storage', serializeData);
 }
 console.log(serializeDataModel);
-
-function readDataModel(): Data {
+function readDataModel() {
   const readData = localStorage.getItem('data-storage');
   if (readData) {
     const parseData = JSON.parse(readData);
